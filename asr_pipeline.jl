e783f159919799c4628eb42f5eaf325b0f72c867
efuenzalidamunoz <emerson.fuenzalida@alumnos.ucm.cl>
2026-08-17 17:20:21 -0400

    update number of threads
    4 hilos para reproductibilidad

--- asr_pipeline.jl
+++ asr_pipeline.jl
@@ -46,8 +46,7 @@ end
 function transcribe(wav_file::String, model::String)
     base = splitext(basename(wav_file))[1]
     out_prefix = joinpath(OUT_DIR, base)
-    cmd = `$WHISPER_CLI -m $model -f $wav_file -l $LANGUAGE -nt -np -otxt -of $out_prefix`
-
+    cmd = `$WHISPER_CLI -m $model -f $wav_file -l $LANGUAGE -nt -np -otxt -of $out_prefix -t 4` # usar 4 hilos
     elapsed = @elapsed run(pipeline(cmd; stdout=devnull, stderr=devnull))
 
     hypothesis = strip(read(out_prefix * ".txt", String))
